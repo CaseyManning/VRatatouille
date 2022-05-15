ed8fd102e4a5d1bd63dc921c74fec59e04267ac3
Casey Manning <kcgrind@gmail.com>
2022-05-14 21:32:22 -0700

    basic hair locomotion

--- Assets/RatHandsController.cs
+++ Assets/RatHandsController.cs
@@ -13,8 +13,19 @@ public class RatHandsController : MonoBehaviour
     public GameObject leftClawBase;
     public GameObject rightClawBase;
 
+    public GameObject leftHairRoot;
+    public GameObject rightHairRoot;
+
+    public GameObject headAnchor;
+
+    public GameObject leftAnchor;
+    public GameObject rightAnchor;
+
     Vector3 rightBaseOffset;
     Vector3 leftBaseOffset;
+
+    Vector3 rightHairOffset;
+    Vector3 leftHairOffset;
 
     public GameObject centerEye;
 
@@ -22,18 +33,36 @@ public class RatHandsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rightBaseOffset = rightClawBase.transform.position - centerEye.transform.position;
-        leftBaseOffset = leftClawBase.transform.position - centerEye.transform.position;
+        //rightBaseOffset = centerEye.transform.InverseTransformPoint(rightClawBase.transform.position);
+        //leftBaseOffset = centerEye.transform.InverseTransformPoint(leftClawBase.transform.position);
+
+        rightHairOffset = headAnchor.transform.InverseTransformPoint(rightHairRoot.transform.position);
+        leftHairOffset = headAnchor.transform.InverseTransformPoint(leftHairRoot.transform.position);
+
+        //rightHairOffset = rightHairRoot.transform.position - headAnchor.transform.position;
+        //leftHairOffset = leftHairRoot.transform.position - headAnchor.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        leftClawBase.transform.position = centerEye.transform.position + leftBaseOffset;
-        rightClawBase.transform.position = centerEye.transform.position + rightBaseOffset;
+        //leftClawBase.transform.position = centerEye.transform.TransformPoint(leftBaseOffset);
+        //rightClawBase.transform.position = centerEye.transform.TransformPoint(rightBaseOffset);
+
+        leftClawBase.transform.position = leftAnchor.transform.position;
+        rightClawBase.transform.position = rightAnchor.transform.position;
+
+        //leftClawBase.transform.position = centerEye.transform.position + leftBaseOffset;
+        //rightClawBase.transform.position = centerEye.transform.position + rightBaseOffset;
 
         leftRatBone.transform.position = leftHandAnchor.transform.position;
         rightRatBone.transform.position = rightHandAnchor.transform.position;
+
+        //rightHairRoot.transform.position = headAnchor.transform.position + rightHairOffset;
+        //leftHairRoot.transform.position = headAnchor.transform.position + leftHairOffset;
+
+        rightHairRoot.transform.position = headAnchor.transform.TransformPoint(rightHairOffset);
+        leftHairRoot.transform.position = headAnchor.transform.TransformPoint(leftHairOffset);
 
         leftRatBone.transform.rotation = leftHandAnchor.transform.rotation;
         leftRatBone.transform.Rotate(new Vector3(0, 180, 0));
---
+++ Assets/RatLocomotion.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatLocomotion : MonoBehaviour
+{
+    public float rotDeadZone = 0f;
+    public float moveDeadZone = 0.05f;
+
+    public GameObject leftHandAnchor;
+    public GameObject rightHandAnchor;
+
+    public GameObject centerAnchor;
+
+    public float rotSpeed = 20f;
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 left = centerAnchor.transform.InverseTransformPoint(leftHandAnchor.transform.position);
+        Vector3 right = centerAnchor.transform.InverseTransformPoint(rightHandAnchor.transform.position);
+
+        //Vector3 center = centerAnchor.transform.position;
+
+
+
+        float rotationAmt = left.z - right.z;
+
+        if(Mathf.Abs(rotationAmt) > rotDeadZone)
+        {
+            transform.parent.Rotate(new Vector3(0, rotationAmt * Time.deltaTime * rotSpeed, 0));
+        }
+        float moveAmt = (left.z + right.z)/2;
+        if(Mathf.Abs(moveAmt) > moveDeadZone)
+        {
+            transform.parent.Translate(new Vector3(0, 0, moveSpeed * Time.deltaTime * moveAmt));
+        }
+        print(moveAmt);
+    }
+}
