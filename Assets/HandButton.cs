e70d05e504c3d38716c554b7510beb8509d36403
Casey Manning <kcgrind@gmail.com>
2022-05-25 12:27:31 -0700

    grabby hands

--- Assets/HandButton.cs
+++ Assets/HandButton.cs
@@ -21,7 +21,6 @@ public class HandButton : MonoBehaviour
     void Update()
     {
         transform.localPosition = new Vector3(start.x, start.y, transform.localPosition.z);
-        print(transform.localPosition.z);
         if (transform.localPosition.z < start.z - 0.04f)
         {
             transform.localPosition = start + new Vector3(0, 0, -0.04f);
