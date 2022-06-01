6e2e0753ad7969c67810d231b4c2826407c76448
Casey Manning <kcgrind@gmail.com>
2022-05-31 20:46:14 -0700

    rima maze

--- Assets/_Scripts/AudioPlayer.cs
+++ Assets/_Scripts/AudioPlayer.cs
@@ -6,19 +6,6 @@ public class AudioPlayer : MonoBehaviour
 {
     static AudioSource source;
 
-    public class Clips
-    {
-        public static AudioClip ouch;
-        public static AudioClip moveTutorial;
-        public static AudioClip buttonTutorial;
-        public static AudioClip monitorsDialogue1;
-        public static AudioClip monitorsDialogue2;
-        public static AudioClip talkingInElevator;
-        public static AudioClip beforeElevator;
-        public static AudioClip elevatorDing;
-        public static AudioClip outroEnemyDialogue;
-    }
-
     public AudioClip ouch;
     public AudioClip moveTutorial;
     public AudioClip buttonTutorial;
@@ -29,19 +16,13 @@ public class AudioPlayer : MonoBehaviour
     public AudioClip elevatorDing;
     public AudioClip outroEnemyDialogue;
 
+    public static AudioPlayer Clips;
+
     // Start is called before the first frame update
     void Start()
     {
         source = GetComponent<AudioSource>();
-        AudioPlayer.Clips.ouch = ouch;
-        AudioPlayer.Clips.moveTutorial = moveTutorial;
-        AudioPlayer.Clips.buttonTutorial = buttonTutorial;
-        AudioPlayer.Clips.monitorsDialogue1 = monitorsDialogue1;
-        AudioPlayer.Clips.monitorsDialogue2 = monitorsDialogue2;
-        AudioPlayer.Clips.talkingInElevator = talkingInElevator;
-        AudioPlayer.Clips.beforeElevator = beforeElevator;
-        AudioPlayer.Clips.elevatorDing = elevatorDing;
-        AudioPlayer.Clips.outroEnemyDialogue = outroEnemyDialogue;
+        Clips = this;
     }
 
     // Update is called once per frame
